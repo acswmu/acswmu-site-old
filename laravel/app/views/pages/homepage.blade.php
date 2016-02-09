2a72cca0d2ed8959425b956e0ada969e17e331ee
blake <Blakewrege1@gmail.com>
2016-02-09 16:43:27 -0500

    added pm

--- laravel/app/views/pages/homepage.blade.php
+++ laravel/app/views/pages/homepage.blade.php
@@ -6,7 +6,7 @@
 		This is the official website of the Registered Student Organization known as the Academic Computing Society at WMU.
                 Also called the ACS. 
 		The purpose of our organization is to promote networking, academic growth and explore new ideas in the field of computer science.
-		We meet every other week on Wednesday at 7:00-8:00 in room B-115 where we discuss current projects, project ideas and what happened at our last weekend meeting.
+		We meet every other week on Wednesday at 7:00PM in room B-115 where we discuss current projects, project ideas and what happened at our last weekend meeting.
 		For more information on meetings check out our <a href="https://calendar.google.com/calendar/embed?src=acswmu%40gmail.com&ctz=America/New_York"> Google Calender</a>
 		<br> NOTE: we are currently working on updating our website, if you want to help join us on <a href="https://wmu-acs.slack.com/signup">Slack</a>		
 	</p>
